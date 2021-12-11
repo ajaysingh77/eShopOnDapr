8b0687067a0e25ad00fb8bf27d4fbacdae70461e
Ajay Singh <ajay@directechlabs.com>
2021-12-11 17:36:08 +0530

    docker-compose issues partial fix.

---
+++ Create-Container-Apps.ps1
@@ -0,0 +1,45 @@
+﻿$RESOURCE_GROUP="dtl-containerapps"
+$LOCATION="eastus"
+$CONTAINERAPPS_ENVIRONMENT="containerapps-env"
+$LOG_ANALYTICS_WORKSPACE="containerapps-logs"
+$STORAGE_ACCOUNT_CONTAINER="mycontainer"
+
+$STORAGE_ACCOUNT="dtlcontainerappsst"
+
+<#
+az login
+az upgrade
+az extension add `
+  --source https://workerappscliextension.blob.core.windows.net/azure-cli-extension/containerapp-0.2.0-py2.py3-none-any.whl
+az provider register --namespace Microsoft.Web
+
+#>
+
+az group create `
+  --name $RESOURCE_GROUP `
+  --location "$LOCATION"
+
+az monitor log-analytics workspace create `
+  --resource-group $RESOURCE_GROUP `
+  --workspace-name $LOG_ANALYTICS_WORKSPACE
+
+$LOG_ANALYTICS_WORKSPACE_CLIENT_ID=(az monitor log-analytics workspace show --query customerId -g $RESOURCE_GROUP -n $LOG_ANALYTICS_WORKSPACE --out tsv)
+
+$LOG_ANALYTICS_WORKSPACE_CLIENT_SECRET=(az monitor log-analytics workspace get-shared-keys --query primarySharedKey -g $RESOURCE_GROUP -n $LOG_ANALYTICS_WORKSPACE --out tsv)
+
+az containerapp env create `
+  --name $CONTAINERAPPS_ENVIRONMENT `
+  --resource-group $RESOURCE_GROUP `
+  --logs-workspace-id $LOG_ANALYTICS_WORKSPACE_CLIENT_ID `
+  --logs-workspace-key $LOG_ANALYTICS_WORKSPACE_CLIENT_SECRET `
+  --location "$LOCATION"
+
+az storage account create `
+  --name $STORAGE_ACCOUNT `
+  --resource-group $RESOURCE_GROUP `
+  --location "$LOCATION" `
+  --sku Standard_RAGRS `
+  --kind StorageV2
+
+az group delete `
+    --resource-group $RESOURCE_GROUP
